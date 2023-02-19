a8912c4f09d0b4318ee9720b0e771a04a2bcfb8a
wilkin trinidad <81598320+wilkin547@users.noreply.github.com>
2023-02-19 16:41:32 -0400

    Update server.js
    agregue mis aprendizajes

--- aprendiendo a crear y consumir apis/server.js
+++ aprendiendo a crear y consumir apis/server.js
@@ -22,3 +22,13 @@ app.get(uri,(req,res)=>{
 app.listen(port,function(req,res){
     
 })
+
+/*this a repositori about what i have learn of every class SOMETHING THAT I HACE LEARN,
+
+try to check correctly the URL in the App.get
+
+the fecth funtion return a promise
+
+when i have to use res.send() i have to put a string something
+
+the send funtion is the object that return in the fetch/*
